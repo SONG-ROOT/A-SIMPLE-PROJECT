7682f6cdeeda5dfc6428561d7797f049bc3792c4
SONG <66151844+SONG-ROOT@users.noreply.github.com>
2020-06-11 19:03:29 +0800

    Add files via upload

--- MUSIC PPROJECT/URL_1.py
+++ MUSIC PPROJECT/URL_1.py
@@ -28,6 +28,13 @@ def json_wrap(a):#数据清洗 传入JSON ,RETURN歌曲基本信息
             jp1=js['songs'][0]['artists'][0]['name']#艺人名
             jp2=js['songs'][0]['name']#曲名
             jp3=js['songs'][0]['album']['name']#专辑名
+
+            if jp1 is None or len(jp1)==0 :
+                jp1=' '
+            if jp2 is None or len(jp2)==0 :
+                jp2=' '
+            if jp3 is None or len(jp3)==0 :
+                jp3=' '
             jpeg=jp1+','+jp2+','+jp3
             return jpeg
     except Exception as r:
@@ -48,10 +55,14 @@ def json_wrap_tras(a):#数据清洗 传入JSON ,RETURN 歌词和翻译
     try:
         js=json.loads(a)
         jp1=js['lrc']['lyric']#歌词
+        if not 'lyric' in js['tlyric']:
+            return [jp1,'没有翻译且大概率是无时间戳歌词']
         jp2=js['tlyric']['lyric']#翻译
+        
         if jp2 is None:
             jp2='无翻译'
         jpg=[jp1,jp2]
+
         return jpg
     except Exception as r:
         if 'nolyric' in js:
